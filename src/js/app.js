6c5e2003135dab22eaa2f6e7365efaf4eeddc759
Timur Atalay <AIUXW77@amway.com>
2020-06-24 13:46:07 -0700

    Show wrapper after DCL

--- src/js/app.js
+++ src/js/app.js
@@ -14,6 +14,7 @@ const $ = (id) => document.getElementById(id);
         set: (key, value) => localStorage.setItem(key, JSON.stringify(value))
     };
 
+    // Is Node?
     var userAgent = navigator.userAgent.toLowerCase();
     var isNode = userAgent.indexOf(' electron/') > -1;
 
@@ -29,13 +30,13 @@ const $ = (id) => document.getElementById(id);
             $('header-mac').remove();
             $('header-win').style.display = 'block';
             $('header-win-title').innerHTML = appName;
-    
+
             if (ipc.sendSync('isNormal')) $('unmax').style.display = 'none';
             if (ipc.sendSync('isMaximized')) $('max').style.display = 'none';
             ipc.on('fullscreen', (event, isFullscreen) => {
                 if (isFullscreen) $('max').click();
             });
-    
+
             $('winButtons').addEventListener('click', (e) => {
                 switch (e.target.id) {
                     case 'min':
@@ -68,6 +69,9 @@ const $ = (id) => document.getElementById(id);
         $('header-mac-title').innerHTML = appName;
     }
 
+    // Load last calculations
+    $('input').value = ls.get('input');
+
     // App default settings
     const defaultSettings = {
         currencies: true,
@@ -85,7 +89,6 @@ const $ = (id) => document.getElementById(id);
         resizable: true,
         thouSep: true
     };
-
     Object.freeze(defaultSettings);
 
     // Initiate app settings
@@ -108,28 +111,25 @@ const $ = (id) => document.getElementById(id);
 
     function applySettings() {
         settings = ls.get('settings');
-        var theme = ls.get('theme');
-        var elements = document.getElementsByName('sync');
 
+        var theme = ls.get('theme');
         $('style').setAttribute('href', theme == 'dark' ? 'dark.css' : 'light.css');
         $('input').setAttribute('wrap', settings.lineWrap ? 'on' : 'off');
 
+        var elements = document.getElementsByName('sync');
         for (var el of elements) {
             el.style.fontSize = settings.fontSize;
             el.style.fontWeight = settings.fontWeight;
         }
+
         $('lineNo').style.display = settings.lineNumbers ? 'block' : 'none';
         $('inputPane').style.width = settings.resizable ? settings.inputWidth + '%' : defaultSettings.inputWidth;
         $('inputPane').style.marginLeft = settings.lineNumbers ? '0px' : '18px';
         $('output').style.textAlign = settings.resizable ? 'left' : 'right';
         $('handle').style.display = settings.resizable ? 'block' : 'none';
-        $('wrapper').style.visibility = 'visible';
 
         calculate();
     }
-
-    // Load last calculations
-    $('input').value = ls.get('input');
 
     // Apply theme and settings
     feather.replace();
@@ -137,6 +137,7 @@ const $ = (id) => document.getElementById(id);
     applySettings();
 
     document.addEventListener('DOMContentLoaded', () => {
+        $('wrapper').style.visibility = 'visible';
         $('input').focus();
         $('input').addEventListener('input', calculate);
         $('input').dispatchEvent(new CustomEvent('scroll'));
@@ -316,7 +317,7 @@ const $ = (id) => document.getElementById(id);
                         populateSaved();
                     });
                     break;
-                case 'darkModeButton':
+                case 'darkModeButton': // Set theme
                     ls.set('theme', $('darkModeButton').checked ? 'dark' : 'light');
                     if (isNode) ipc.send('darkMode', $('darkModeButton').checked);
                     applyTheme();
@@ -340,14 +341,14 @@ const $ = (id) => document.getElementById(id);
                         }
                     });
                     break;
-                case 'sizeReset':
+                case 'sizeReset': // Reset panel size
                     settings.inputWidth = defaultSettings.inputWidth;
                     $('sizeReset').style.display = 'none';
                     ls.set('settings', settings);
                     applySettings();
                     $('defaultSettingsButton').style.display = JSON.stringify(settings) == JSON.stringify(defaultSettings) ? 'none' : 'block';
                     break;
-                case 'currencyButton': // Update exchange rates
+                case 'currencyButton': // Enable currency rates
                     if (settings.currencies) {
                         $('currencyUpdate').style.display = $('currencyButton').checked ? "block" : "none";
                     }
@@ -398,6 +399,8 @@ const $ = (id) => document.getElementById(id);
                     ls.set('settings', settings);
                     plot();
                     break;
+
+                    // Load demo
                 case 'demoButton':
                     ls.set('undoData', $('input').value);
                     $('input').value = demo;
@@ -673,56 +676,56 @@ const $ = (id) => document.getElementById(id);
     });
 
     var demo = '# In addition to all math.js features you can do:\n' +
-    '# Subtotal all numbers in a block\n' +
-    '3+5\n' +
-    '8*2\n' +
-    'subtotal\n' +
-    '\n' +
-    '# Total everything up to this point\n' +
-    'total\n' +
-    '\n' +
-    '# Average everything up to this point\n' +
-    'avg\n' +
-    '\n' +
-    '# Dates & Times\n' +
-    'today\n' +
-    'today + 1 day + 2 weeks\n' +
-    'today + 3 days\n' +
-    'today - 2 weeks\n' +
-    'today + 5 years\n' +
-    '5/8/2019 + 1 week\n' +
-    'May 8, 2019 - 2 months\n' +
-    '\n' +
-    'now\n' +
-    'now + 2 hours\n' +
-    'now + 48 hours\n' +
-    'ans + 30 minutes\n' +
-    '\n' +
-    '# ans token\n' +
-    'ans + 5 days\n' +
-    '2+2\n' +
-    'ans * 5\n' +
-    '\n' +
-    '# line# token\n' +
-    'line30 * 5 / 2\n' +
-    'line28 + 10 days\n' +
-    '\n' +
-    '# Percentages\n' +
-    '5% of 100\n' +
-    '100 + 5%\n' +
-    '100 + 25%%4 + 1\n' +
-    '100 + 20% of 100 + 10%3 - 10%\n' +
-    '(100 + 20%)% of 80 + 10%3 - 10%\n' +
-    '120% of 80 + (10%3 - 10%)\n' +
-    'line29% of 80\n' +
-    'line30 - ans%\n' +
-    'line39 + line38%\n' +
-    '\n' +
-    '# Currencies (data from floatrates.com)\n' +
-    '1 USD to EUR\n' +
-    '25 EUR to CAD\n' +
-    '\n' +
-    '# Plot functions\n' +
-    'f(x) = sin(x)\n' +
-    'f(x) = 2x^2 + 3x -5\n';
+        '# Subtotal all numbers in a block\n' +
+        '3+5\n' +
+        '8*2\n' +
+        'subtotal\n' +
+        '\n' +
+        '# Total everything up to this point\n' +
+        'total\n' +
+        '\n' +
+        '# Average everything up to this point\n' +
+        'avg\n' +
+        '\n' +
+        '# Dates & Times\n' +
+        'today\n' +
+        'today + 1 day + 2 weeks\n' +
+        'today + 3 days\n' +
+        'today - 2 weeks\n' +
+        'today + 5 years\n' +
+        '5/8/2019 + 1 week\n' +
+        'May 8, 2019 - 2 months\n' +
+        '\n' +
+        'now\n' +
+        'now + 2 hours\n' +
+        'now + 48 hours\n' +
+        'ans + 30 minutes\n' +
+        '\n' +
+        '# ans token\n' +
+        'ans + 5 days\n' +
+        '2+2\n' +
+        'ans * 5\n' +
+        '\n' +
+        '# line# token\n' +
+        'line30 * 5 / 2\n' +
+        'line28 + 10 days\n' +
+        '\n' +
+        '# Percentages\n' +
+        '5% of 100\n' +
+        '100 + 5%\n' +
+        '100 + 25%%4 + 1\n' +
+        '100 + 20% of 100 + 10%3 - 10%\n' +
+        '(100 + 20%)% of 80 + 10%3 - 10%\n' +
+        '120% of 80 + (10%3 - 10%)\n' +
+        'line29% of 80\n' +
+        'line30 - ans%\n' +
+        'line39 + line38%\n' +
+        '\n' +
+        '# Currencies (data from floatrates.com)\n' +
+        '1 USD to EUR\n' +
+        '25 EUR to CAD\n' +
+        '\n' +
+        '# Plot functions\n' +
+        'f(x) = sin(x)\n' +
+        'f(x) = 2x^2 + 3x -5\n';
 })();
