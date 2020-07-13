bcca62119e3ab7eed2fbb5a1b9a1996df0e96381
Timur Atalay <AIUXW77@amway.com>
2020-07-13 15:24:21 -0700

    Bug fixes and improvements

--- src/js/app.js
+++ src/js/app.js
@@ -103,7 +103,28 @@ var cm = CodeMirror.fromTextArea($('inputArea'), {
     }
 
     feather.replace();
-    document.getElementsByClassName('CodeMirror-scroll')[0].setAttribute('name', 'sync');
+
+    // Sync scroll
+    var inputScroll = false;
+    var outputScroll = false;
+    var leftSide = document.getElementsByClassName('CodeMirror-scroll')[0];
+    var rightSide = $('output');
+
+    leftSide.addEventListener('scroll', () => {
+        if (!inputScroll) {
+            outputScroll = true;
+            rightSide.scrollTop = leftSide.scrollTop;
+        }
+        inputScroll = false;
+    });
+
+    rightSide.addEventListener('scroll', () => {
+        if (!outputScroll) {
+            inputScroll = true;
+            leftSide.scrollTop = rightSide.scrollTop;
+        }
+        outputScroll = false;
+    });
 
     // App settings
     const defaultSettings = {
@@ -559,7 +580,7 @@ var cm = CodeMirror.fromTextArea($('inputArea'), {
     $('precisionRange').addEventListener('input', () => $('precision-label').innerHTML = $('precisionRange').value);
 
     // Help dialog content
-    $('searchBox').addEventListener('input', (e) => {
+    $('searchBox').addEventListener('input', () => {
         var str = $('searchBox').value.trim();
         if (str) {
             try {
@@ -693,50 +714,6 @@ var cm = CodeMirror.fromTextArea($('inputArea'), {
         setTimeout(() => $('msg').style.opacity = '0', 3000);
         setTimeout(() => $('msg').innerHTML = '', 3500);
     }
-
-    /**
-     * @fileoverview syncscroll - scroll several areas simultaniously
-     * @version 0.0.3
-     * 
-     * @license MIT, see http://github.com/asvd/intence
-     * @copyright 2015 asvd <heliosframework@gmail.com>
-     * 
-     * Modified by Timur Atalay
-     */
-    (() => {
-        var names = {};
-        var elems = document.getElementsByName('sync');
-        var i, j, el, found, name;
-        var scrollSync = (el, name) => {
-            el.addEventListener('scroll', el.syn = () => {
-                var elems = names[name];
-                var scrollY = el.scrollTop;
-                var yRate = scrollY / (el.scrollHeight - el.clientHeight);
-                var updateY = scrollY != el.eY;
-
-                el.eY = scrollY;
-                for (i in elems) {
-                    var otherEl = elems[i++];
-                    if (otherEl != el) {
-                        if (updateY && Math.round(otherEl.scrollTop - (scrollY = otherEl.eY = Math.round(yRate * (otherEl.scrollHeight - otherEl.clientHeight))))) {
-                            otherEl.scrollTop = scrollY;
-                        }
-                    }
-                }
-            }, 0);
-        };
-
-        for (i = 0; i < elems.length;) {
-            found = j = 0;
-            el = elems[i++];
-            if (!(name = el.getAttribute('name'))) continue;
-            el = el.scroller || el;
-            for (j in (names[name] = names[name] || [])) found |= names[name][j++] == el;
-            if (!found) names[name].push(el);
-            el.eX = el.eY = 0;
-            scrollSync(el, name);
-        }
-    })();
 
     // Mousetrap
     var traps = {
