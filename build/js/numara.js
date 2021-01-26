ef0b3a90771cf80d042488f288df8ed29f293ab5
Timur Atalay <timur@atalay.io>
2021-01-25 22:30:13 -0800

    Minor changes

--- build/js/numara.js
+++ build/js/numara.js
@@ -7,7 +7,7 @@
 const appInfo = {
     productName: 'Numara',
     description:'Numara Calculator',
-    version: '2.18.6',
+    version: '2.18.7',
     author: 'Timur Atalay',
     homepage: 'https://github.com/bornova/numara-calculator',
     licence: 'MIT',
