5ed38e2e63b1784656eb7334e4d7efd20b0f9283
Timur <35872220+bornova@users.noreply.github.com>
2021-01-28 14:16:11 -0800

    Minor UI fix

--- build/js/numara.js
+++ build/js/numara.js
@@ -7,7 +7,7 @@
 const appInfo = {
     productName: 'Numara',
     description:'Numara Calculator',
-    version: '2.19.2',
+    version: '2.19.3',
     author: 'Timur Atalay',
     homepage: 'https://github.com/bornova/numara-calculator',
     licence: 'MIT',
