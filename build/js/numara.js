1613553f178de1b5f668447c30d3ab7805c7e967
Bornova <35872220+bornova@users.noreply.github.com>
2022-03-08 21:00:28 -0800

    Build v3.11.4

--- build/js/numara.js
+++ build/js/numara.js
@@ -7,7 +7,7 @@
 appInfo = {
     productName: 'Numara',
     description:'Numara Calculator',
-    version: '3.11.3',
+    version: '3.11.4',
     author: 'Timur Atalay',
     homepage: 'https://github.com/bornova/numara-calculator',
     licence: 'MIT',
