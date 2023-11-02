b64ce15db7f5e9b359e5aa910b6c1ad2abee88c9
Timur Atalay <timur@atalay.io>
2023-11-02 12:46:40 -0700

    Cleanup preload.js

--- src/preload.cjs
+++ src/preload.cjs
@@ -3,121 +3,59 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('numara', {
   // App theme
   isDark: () => ipcRenderer.sendSync('isDark'),
-  setTheme: (theme) => {
-    ipcRenderer.send('setTheme', theme)
-  },
-  themeUpdate: (callback) => {
-    ipcRenderer.on('themeUpdate', callback)
-  },
+  setTheme: (theme) => ipcRenderer.send('setTheme', theme),
+  themeUpdate: (callback) => ipcRenderer.on('themeUpdate', callback),
 
   // Window controls
-  fullscreen: () => {
+  fullscreen: () =>
     ipcRenderer.on('fullscreen', (event, isFullscreen) => {
       if (isFullscreen) {
         ipcRenderer.send('maximize')
       }
-    })
-  },
-  setOnTop: (callback) => {
-    ipcRenderer.send('setOnTop', callback)
-  },
-  close: () => {
-    ipcRenderer.send('close')
-  },
-  minimize: () => {
-    ipcRenderer.send('minimize')
-  },
-  maximize: () => {
-    ipcRenderer.send('maximize')
-  },
-  unmaximize: () => {
-    ipcRenderer.send('unmaximize')
-  },
-  isMax: (callback) => {
-    ipcRenderer.on('isMax', callback)
-  },
+    }),
+  setOnTop: (callback) => ipcRenderer.send('setOnTop', callback),
+  close: () => ipcRenderer.send('close'),
+  minimize: () => ipcRenderer.send('minimize'),
+  maximize: () => ipcRenderer.send('maximize'),
+  unmaximize: () => ipcRenderer.send('unmaximize'),
+  isMax: (callback) => ipcRenderer.on('isMax', callback),
   isMaximized: () => ipcRenderer.sendSync('isMaximized'),
   isResized: () => ipcRenderer.sendSync('isResized'),
-  resetSize: () => {
-    ipcRenderer.send('resetSize')
-  },
+  resetSize: () => ipcRenderer.send('resetSize'),
 
   // Print
-  print: () => {
-    ipcRenderer.send('print')
-  },
-  printReply: (callback) => {
-    callback()
-  },
+  print: () => ipcRenderer.send('print'),
+  printReply: (callback) => callback(),
 
   // Import
-  import: () => {
-    ipcRenderer.send('import')
-  },
-  importData: (callback) => {
-    ipcRenderer.on('importData', callback)
-  },
-  importDataError: (callback) => {
-    ipcRenderer.on('importDataError', callback)
-  },
+  import: () => ipcRenderer.send('import'),
+  importData: (callback) => ipcRenderer.on('importData', callback),
+  importDataError: (callback) => ipcRenderer.on('importDataError', callback),
 
   //Export
-  export: (arg1, arg2) => {
-    ipcRenderer.send('export', arg1, arg2)
-  },
-  exportData: (callback) => {
-    ipcRenderer.on('exportData', callback)
-  },
-  exportDataError: (callback) => {
-    ipcRenderer.on('exportDataError', callback)
-  },
+  export: (arg1, arg2) => ipcRenderer.send('export', arg1, arg2),
+  exportData: (callback) => ipcRenderer.on('exportData', callback),
+  exportDataError: (callback) => ipcRenderer.on('exportDataError', callback),
 
   // Context menus
-  inputContextMenu: (index, isEmpty, isLine, isSelection, isMultiLine, hasAnswer) => {
-    ipcRenderer.send('inputContextMenu', index, isEmpty, isLine, isSelection, isMultiLine, hasAnswer)
-  },
-  outputContextMenu: (index, isEmpty, hasAnswer) => {
-    ipcRenderer.send('outputContextMenu', index, isEmpty, hasAnswer)
-  },
-  textboxContextMenu: () => {
-    ipcRenderer.send('textboxContextMenu')
-  },
+  inputContextMenu: (index, isEmpty, isLine, isSelection, isMultiLine, hasAnswer) =>
+    ipcRenderer.send('inputContextMenu', index, isEmpty, isLine, isSelection, isMultiLine, hasAnswer),
+  outputContextMenu: (index, isEmpty, hasAnswer) => ipcRenderer.send('outputContextMenu', index, isEmpty, hasAnswer),
+  textboxContextMenu: () => ipcRenderer.send('textboxContextMenu'),
 
-  copyLine: (callback) => {
-    ipcRenderer.on('copyLine', callback)
-  },
-  copyAnswer: (callback) => {
-    ipcRenderer.on('copyAnswer', callback)
-  },
-  copyLineWithAnswer: (callback) => {
-    ipcRenderer.on('copyLineWithAnswer', callback)
-  },
-  copyAllLines: (callback) => {
-    ipcRenderer.on('copyAllLines', callback)
-  },
-  copyAllAnswers: (callback) => {
-    ipcRenderer.on('copyAllAnswers', callback)
-  },
-  copyAll: (callback) => {
-    ipcRenderer.on('copyAll', callback)
-  },
+  copyAll: (callback) => ipcRenderer.on('copyAll', callback),
+  copyAllLines: (callback) => ipcRenderer.on('copyAllLines', callback),
+  copyAllAnswers: (callback) => ipcRenderer.on('copyAllAnswers', callback),
+  copyLine: (callback) => ipcRenderer.on('copyLine', callback),
+  copyAnswer: (callback) => ipcRenderer.on('copyAnswer', callback),
+  copyLineWithAnswer: (callback) => ipcRenderer.on('copyLineWithAnswer', callback),
 
   // Update app
-  checkUpdate: () => {
-    ipcRenderer.send('checkUpdate')
-  },
-  notifyUpdate: (callback) => {
-    ipcRenderer.on('notifyUpdate', callback)
-  },
-  updateApp: () => {
-    ipcRenderer.send('updateApp')
-  },
-  updateStatus: (callback) => {
-    ipcRenderer.on('updateStatus', callback)
-  },
+  updateApp: () => ipcRenderer.send('updateApp'),
+  checkUpdate: () => ipcRenderer.send('checkUpdate'),
+  notifyUpdate: (callback) => ipcRenderer.on('notifyUpdate', callback),
+  updateStatus: (callback) => ipcRenderer.on('updateStatus', callback),
 
   // Reset
-  resetApp: () => {
-    ipcRenderer.send('resetApp')
-  }
+  resetApp: () => ipcRenderer.send('resetApp')
 })
