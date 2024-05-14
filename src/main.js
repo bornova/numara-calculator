27a96965a3757f8478cdb6b3950a25b38ea993f2
Timur Atalay <timur@atalay.io>
2024-05-13 21:45:41 -0700

    Update log start message

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import * as fs from 'node:fs'
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 
-log.info(`Starting Numara... (${app.isPackaged ? 'Prod' : 'Dev'})`)
+log.info(`Starting Numara... [v${app.getVersion()}] ${app.isPackaged ? '' : '(Dev)'}`)
 log.initialize({ spyRendererConsole: true })
 log.errorHandler.startCatching()
 log.eventLogger.startLogging()
